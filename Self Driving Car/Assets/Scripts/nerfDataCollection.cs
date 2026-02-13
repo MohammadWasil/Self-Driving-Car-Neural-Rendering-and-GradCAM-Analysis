5c916999d092d2c0462c06d01c8f79faf50b688b
MohammadWasil <wasilmohd1@gmail.com>
2026-02-13 01:32:01 +0100

    implement Unity-to-NeRF coordinate transformation for camera matrices again

---
+++ Self Driving Car/Assets/Scripts/nerfDataCollection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nerfDataCollection : MonoBehaviour
+{
+
+    int currentFrame = 0;
+
+    public Camera cam;
+
+    private Matrix4x4 m;
+    public Matrix4x4 c2w;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentFrame % 5 == 0)
+        {
+            // capture the screen
+
+            // get the camera transform - position and rotation
+            m = cam.cameraToWorldMatrix;
+            c2w = m.inverse;
+
+            for (int i = 0; i < 4; i++) {
+                c2w[i, 1] *= -1; // Flip Y
+                c2w[i, 2] *= -1; // Flip Z
+            }
+
+
+
+        }
+
+
+    }
+}
